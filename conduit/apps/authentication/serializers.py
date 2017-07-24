61851ac1cf0e797bcda4beb74b4750e404aa2bd5
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-07-23 17:04:14 -0700

    added ability to show and update users.

--- conduit/apps/authentication/serializers.py
+++ conduit/apps/authentication/serializers.py
@@ -57,6 +57,31 @@ class LoginSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email', 'username', 'password', 'token']
 
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length =  128,
+        min_length = 8,
+        write_only=True
+    )
+
+    class Meta:
+        model = 'User'
+        fields = ['email', 'username', 'password', 'token']
+        read_only_fields = ('token',)
+    
+    def update(self, instance, validated_data):
+        password = validated_data('password', None)
+
+    
+        for (key, value) in validated_data.items():
+            setattrr(instance, key, value)
+        
+        if password is not None:
+            instance.set_password(password)
+        
+        instance.save()
+
+        return instance
 
 
 
--- conduit/apps/authentication/views.py
+++ conduit/apps/authentication/views.py
@@ -1,9 +1,10 @@
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import LoginSerializer, RegistrationSerializer
+from .serializers import LoginSerializer, RegistrationSerializer, UserSerializer
 from .renderers import UserJSONRenderer
 
 class RegistrationAPIView(APIView):
@@ -33,4 +34,25 @@ class LoginAPIView(APIView):
         user = request.data.get('user', {})
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = UserSerializer
+
+    def retrive(self, request, *args, **kwargs):
+        serializer = self.seralizer_class(request.user)
+
+        return Response(serializer.data, status= status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        serializer_data = request.data.get('user', {})
+
+        serializer = self.serializer_class(
+            request.user, data=serializer_data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
         return Response(serializer.data, status=status.HTTP_200_OK)
