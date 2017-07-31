6dfd140ac139059a600241950c53589e831ece91
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-07-30 23:28:46 -0700

    using timestamp model in profile and user models.

--- conduit/apps/authentication/models.py
+++ conduit/apps/authentication/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     )
 
 from django.db import models
+from conduit.apps.core.models import TimestampedModel
 
 class UserManager(BaseUserManager):
 
@@ -35,13 +36,11 @@ class UserManager(BaseUserManager):
 
         return user
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin, TimestampedModel):
     username = models.CharField(db_index=True, max_length=255, unique=True)
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
---
+++ conduit/apps/profiles/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from conduit.apps.models import TimestampModel
+
+class Profile(TimestampModel):
+    user = models.OneToOneField(
+        'authentication.User', on_delete=models.CASCADE)
+    
+    bio = models.TextField(blank=True)
+
+    image = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
