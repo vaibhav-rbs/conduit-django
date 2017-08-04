ec3cb4a01ccced19bdd7bad4c029655be1d47916
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-03 22:57:12 -0700

    added Article model and it's relevant migrations

--- conduit/apps/articles/models.py
+++ conduit/apps/articles/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 from conduit.apps.core.models import TimestampModel
+
+class Article(TimestampModel):
+    slug = models.SlugField(db_index=True, max_length=255, unique=True)
+    title = models.SlugField(db_index=True, max_length=255)
+
+    description = models.TextField()
+    body = models.TextField()
+
+    author = models.ForeignKey(
+        'profiles.Profile', on_delete=models.CASCADE, related_name='articles')
+    
+    def __str__(self):
+        return self.title
