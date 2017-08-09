a64e7785518baaec872d9efb2a2457d1ace4969d
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-09 00:06:43 -0700

    made exceptions more generic(lolz i know it's funny)

--- conduit/apps/core/exceptions.py
+++ conduit/apps/core/exceptions.py
@@ -4,6 +4,7 @@ def core_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
     handlers = {
+        'NotFound' : _handle_not_found_error,
         'ProfileDoesNotExist': _handle_generic_error,
         'ValidationError': _handle_generic_error
     }
@@ -24,3 +25,18 @@ def _handle_generic_error(exc, context, response):
     
     return response
 
+def _handle_not_found_error(exc, context, response):
+    view = context.get('view', None)
+
+    if view and hasattr(view, 'queryset') and view.queryset is not None:
+        error_key = view.queryset.model._meta.verbose_name
+
+        response.data ={
+            'errors':{
+                error_key: response.data['detail']
+            }
+        }
+    else:
+        response = _handle_generic_error(exc, context, response)
+    
+    return response
