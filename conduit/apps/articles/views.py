03570e2f623640ce7e98fc2b23a5ed4711ef8adf
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-09 00:07:50 -0700

    added  PUT and GET ability for articles.

--- conduit/apps/articles/views.py
+++ conduit/apps/articles/views.py
@@ -1,4 +1,5 @@
 from rest_framework import mixins, status, viewsets
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
@@ -6,7 +7,9 @@ from .models import Article
 from .renderers import ArticleJSONRenderer
 from .serializers import ArticleSerializer
 
-class ArticleViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+class ArticleViewSet(mixins.CreateModelMixin,mixins.ListModelMixin,
+                    mixins.RetrieveModelMixin ,viewsets.GenericViewSet):
+    lookup_field = 'slug'
     queryset = Article.objects.select_related('author','author__user')
     permission_classes = (IsAuthenticatedOrReadOnly,)
     renderer_classes = (ArticleJSONRenderer,)
@@ -23,3 +26,29 @@ class ArticleViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
         serializer.save()
 
         return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+    def update(self, request, slug):
+        try:
+            serializer_instance = self.queryset.get(slug=slug)
+        except Article.DoesNotExist:
+            raise NotFound('An article with this slug does not exist.')
+        
+        serializer_data = request.data.get('article', {})
+        serializer = self.serializer_class(
+            serializer_instance, data=serializer_data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, slug):
+        try:
+            serializer_instance = self.queryset.get(slug=slug)
+        except Article.DoesNotExist:
+            raise NotFound('An article with this slug does not exist.')
+        
+        serializer = self.serializer_class(serializer_instance)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
