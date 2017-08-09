5727fbbd1cbbc56e907f599cdab8621a811431ed
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-09 00:08:42 -0700

    used generic expetion handler.

--- conduit/apps/profiles/views.py
+++ conduit/apps/profiles/views.py
@@ -1,25 +1,24 @@
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from .exceptions import ProfileDoesNotExist
 from .models import Profile
 from .renderers import ProfileJSONRenderer
 from .serializers import ProfileSerializer
 
 class ProfileRetrieveAPIView(RetrieveAPIView):
     permission_classes = (AllowAny,)
+    queryset = Profile.objects.select_related('user')
     renderer_classes = (ProfileJSONRenderer,)
     serializer_class = ProfileSerializer
 
     def retrieve(self, request, username, *args, **kwargs):
         try:
-            profile = Profile.objects.select_related('user').get(
-                user__username=username
-            )
+            profile = self.queryset.get(user__username=username)
         except Profile.DoesNotExist:
-            raise ProfileDoesNotExist
+            raise NotFound('A profile with this username does not exist.')
         
         serializer = self.serializer_class(profile)
 
