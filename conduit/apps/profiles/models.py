d51c3f3e8a97b8bf81655f88d02282cddfe9840d
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-13 00:31:53 -0700

    comments.

--- conduit/apps/profiles/models.py
+++ conduit/apps/profiles/models.py
@@ -10,5 +10,20 @@ class Profile(TimestampedModel):
 
     image = models.URLField(blank=True)
 
+    follows = models.ManyToManyField(
+        'self', related_name='followed_by', symmetrical=False )
+
     def __str__(self):
         return self.user.username
+
+    def follow(self, profile):
+        self.follows.add(profile)
+    
+    def unfollow(self, profile):
+        self.follow.remove(profile)
+    
+    def is_following(self, profile):
+        return self.follows.filter(pk=profile.pk).exists()
+
+    def is_followed_by(self, profile):
+        return self.followed_by.filter(pk=profile.pk).exists()
